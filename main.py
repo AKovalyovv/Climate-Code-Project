6618979ff8d5dd413728f2c43adae41ec93563be
SugarBone <aleksandr.kovalyovv@gmail.com>
2021-08-07 18:57:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import scraper
+import filter
 
 htmllist = scraper.pagedownload("https://www.goodwork.ca/volunteer/")
 hreflist = scraper.linkscraper(htmllist)
 finallist = scraper.infoscraper(hreflist)
+filteredlist = filter.filterbylocation(finallist, "Toronto")
+
