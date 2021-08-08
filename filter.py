0c00b7b65518775439a874666075ba6b5147b964
SugarBone <aleksandr.kovalyovv@gmail.com>
2021-08-08 05:41:16 -0700

    Removed unnecessary scripts

--- filter.py
+++
@@ -1,8 +0,0 @@
-from scraper import VolunteerPosting
-
-def filterbylocation(datalist, location):
-	filteredlist = []
-	for posting in datalist:
-		if location.lower() in posting.location.lower():
-			filteredlist.append(posting)
-	return filteredlist
--- main.py
+++
@@ -1,8 +0,0 @@
-import scraper
-import filter
-
-htmllist = scraper.pagedownload("https://www.goodwork.ca/volunteer/")
-hreflist = scraper.linkscraper(htmllist)
-finallist = scraper.infoscraper(hreflist)
-filteredlist = filter.filterbylocation(finallist, "Toronto")
-
