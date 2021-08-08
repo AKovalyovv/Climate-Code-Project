2cd72dd2a36acfaf0fb37f109f6a6b13e641d695
samlukas <samuelplukas@gmail.com>
2021-08-07 23:41:16 -0700

    Added JavaScript, Jobs now show

--- EnviroBoard/static/home.js
+++ EnviroBoard/static/home.js
@@ -1,4 +1,4 @@
-alert('The home.js file has been loaded');
+// alert('The home.js file has been loaded');
 var jobs = [];
 
 function initialize() {
@@ -10,7 +10,52 @@ function initialize() {
 		jobs = responseJson;
 		console.log(jobs);
 		console.log(jobs[0]);
+	})
+	.then(() => {
+		var ul = document.getElementById("header");
+		jobs.forEach(job => {
+  			ul.appendChild(createListItem(job));
+		});
 	});
+}
+
+/* Constructs this html element and returns it:
+<a href="http://google.ca/">
+	<div class="column">
+		<h2>Job Name</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+		<p>Location</p>
+	</div>
+</a>
+*/
+function createListItem(job) {
+	var wrapper = document.createElement('a');
+	wrapper.href = job.link;
+	var div = document.createElement('div');
+	div.className = "column";
+	var jobName = document.createElement('h2');
+	jobName.textContent = job.name;
+	var description = document.createElement('p');
+	description.textContent = job.organization;
+	var location = document.createElement('p');
+	location.textContent = job.location;
+
+	div.appendChild(jobName);
+	div.appendChild(description);
+	div.appendChild(location);
+	wrapper.appendChild(div);
+
+	return wrapper;
+}
+
+/*
+	Pulls jobs based on the location
+	
+*/
+function submitForm() {
+	console.log("form was submitted");
+	
+	return false;
 }
 
 window.onload = initialize();
